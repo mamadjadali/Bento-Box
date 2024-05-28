eeb14e92f4ff0c3980fcbb7fc6abf21b88d89311
Mamad Jadali <jadalimamad@gmail.com>
2024-05-28 07:48:58 +0330

    fix contact link

--- src/components/ui/Grid-six.jsx
+++ src/components/ui/Grid-six.jsx
@@ -1,5 +1,4 @@
 import Contactme from "../../data/Contactme.json"
-import Social from "../../data/Social.json"
 
 const GridSix = () => {
   return (
@@ -11,12 +10,12 @@ const GridSix = () => {
 		</div>
 			<div className="absolute left-0 pb-9 pl-11 lg:pl-14 flex flex-col items-center p-3 text-white">
 			<div className="w-14 h-14 rounded-md rounded-b-none bg-neutral-50/5 hover:bg-neutral-50/10 hover:border-gray-300/90 duration-200 backdrop-blur border border-gray-400/30 mr-2 flex justify-center items-center">
-				<a href={Social.Phone}>
+				<a href="tel:+989355473072">
 				<svg className="w-6 h-6" stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" height="200px" width="200px" xmlns="http://www.w3.org/2000/svg"><path d="M2 8.99374C2 5.68349 4.67654 3 8.00066 3H15.9993C19.3134 3 22 5.69478 22 8.99374V21H8.00066C4.68659 21 2 18.3052 2 15.0063V8.99374ZM14 11V13H16V11H14ZM8 11V13H10V11H8Z"></path></svg>
 				</a>
 				</div>
 			<div className="w-14 h-14 rounded-md rounded-t-none bg-neutral-50/5 hover:bg-neutral-50/10 hover:border-gray-300/90 duration-200 backdrop-blur border border-gray-400/30 mr-2 flex justify-center items-center">
-				<a href={Social.Email}>
+				<a href="mailto:mohsen.pourbanaei@gmail.com">
 				<svg className="w-5 h-5" stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="200px" width="200px" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="4"></circle><path d="M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-4 8"></path></svg>
 				</a>
 			</div>
