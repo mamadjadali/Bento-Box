bca5b6fbbddaa0286e53cb90c547904b56943f50
Mamad Jadali <jadalimamad@gmail.com>
2024-05-29 21:52:15 +0330

    footer

---
+++ src/components/magicui/Footer.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+const GitHubIcon = (props: React.ComponentPropsWithoutRef<'svg'>) => {
+  return (
+    <svg viewBox="0 0 496 512" aria-hidden="true"  {...props}>
+      <path d="M496 256c0 137-111 248-248 248-25.6 0-50.2-3.9-73.4-11.1 10.1-16.5 25.2-43.5 30.8-65 3-11.6 15.4-59 15.4-59 8.1 15.4 31.7 28.5 56.8 28.5 74.8 0 128.7-68.8 128.7-154.3 0-81.9-66.9-143.2-152.9-143.2-107 0-163.9 71.8-163.9 150.1 0 36.4 19.4 81.7 50.3 96.1 4.7 2.2 7.2 1.2 8.3-3.3.8-3.4 5-20.3 6.9-28.1.6-2.5.3-4.7-1.7-7.1-10.1-12.5-18.3-35.3-18.3-56.6 0-54.7 41.4-107.6 112-107.6 60.9 0 103.6 41.5 103.6 100.9 0 67.1-33.9 113.6-78 113.6-24.3 0-42.6-20.1-36.7-44.8 7-29.5 20.5-61.3 20.5-82.6 0-19-10.2-34.9-31.4-34.9-24.9 0-44.9 25.7-44.9 60.2 0 22 7.4 36.8 7.4 36.8s-24.5 103.8-29 123.2c-5 21.4-3 51.6-.9 71.2C65.4 450.9 0 361.1 0 256 0 119 111 8 248 8s248 111 248 248z"></path>
+    </svg>
+  )
+}
+
+const XIcon = (props: React.ComponentPropsWithoutRef<'svg'>) => {
+  return (
+    <svg viewBox="0 0 20 20" aria-hidden="true" {...props}>
+      <path d="M11.1527 8.92804L16.2525 3H15.044L10.6159 8.14724L7.07919 3H3L8.34821 10.7835L3 17H4.20855L8.88474 11.5643L12.6198 17H16.699L11.1524 8.92804H11.1527ZM9.49748 10.8521L8.95559 10.077L4.644 3.90978H6.50026L9.97976 8.88696L10.5216 9.66202L15.0446 16.1316H13.1883L9.49748 10.8524V10.8521Z" />
+    </svg>
+  )
+}
+
+const DiscordIcon = (props: React.ComponentPropsWithoutRef<'svg'>) => {
+  return (
+    <svg viewBox="0 0 20 20" aria-hidden="true" {...props}>
+      <path d="M16.238 4.515a14.842 14.842 0 0 0-3.664-1.136.055.055 0 0 0-.059.027 10.35 10.35 0 0 0-.456.938 13.702 13.702 0 0 0-4.115 0 9.479 9.479 0 0 0-.464-.938.058.058 0 0 0-.058-.027c-1.266.218-2.497.6-3.664 1.136a.052.052 0 0 0-.024.02C1.4 8.023.76 11.424 1.074 14.782a.062.062 0 0 0 .024.042 14.923 14.923 0 0 0 4.494 2.272.058.058 0 0 0 .064-.02c.346-.473.654-.972.92-1.496a.057.057 0 0 0-.032-.08 9.83 9.83 0 0 1-1.404-.669.058.058 0 0 1-.029-.046.058.058 0 0 1 .023-.05c.094-.07.189-.144.279-.218a.056.056 0 0 1 .058-.008c2.946 1.345 6.135 1.345 9.046 0a.056.056 0 0 1 .059.007c.09.074.184.149.28.22a.058.058 0 0 1 .023.049.059.059 0 0 1-.028.046 9.224 9.224 0 0 1-1.405.669.058.058 0 0 0-.033.033.056.056 0 0 0 .002.047c.27.523.58 1.022.92 1.495a.056.056 0 0 0 .062.021 14.878 14.878 0 0 0 4.502-2.272.055.055 0 0 0 .016-.018.056.056 0 0 0 .008-.023c.375-3.883-.63-7.256-2.662-10.246a.046.046 0 0 0-.023-.021Zm-9.223 8.221c-.887 0-1.618-.814-1.618-1.814s.717-1.814 1.618-1.814c.908 0 1.632.821 1.618 1.814 0 1-.717 1.814-1.618 1.814Zm5.981 0c-.887 0-1.618-.814-1.618-1.814s.717-1.814 1.618-1.814c.908 0 1.632.821 1.618 1.814 0 1-.71 1.814-1.618 1.814Z" />
+    </svg>
+  )
+}
+
+const SocialLink = ({
+  href,
+  icon: Icon,
+  children,
+}: {
+  href: string
+  icon: React.ComponentType<{ className?: string }>
+  children: React.ReactNode
+}) => {
+  return (
+    <a href={href} target="_blank" rel="noreferrer" className="group">
+      <span className="sr-only">{children}</span>
+      <Icon className="h-5 w-5 fill-zinc-700 transition group-hover:fill-gray-100 dark:group-hover:fill-gray-200" />
+    </a>
+  )
+}
+
+const MinimalSocialsFooter = () => {
+  return (
+    <div className="flex w-full flex-col items-center justify-between gap-5 border-t border-gray-900/5 pt-8 pb-8 sm:flex-row dark:border-white/5">
+      <p className="text-xs font-medium text-gray-600 dark:text-gray-100">
+        Copyright © {new Date().getFullYear()}{' '}
+        <a
+          href="https://x.com/mohsenpourbanaei"
+          className="text-blue-500 "
+        >
+          MohsenPB
+        </a>{' '}
+      </p>
+      <div className="flex gap-4">
+        <SocialLink href="https://x.com/mohsenpourbanaei" icon={XIcon}>
+          Follow me on X
+        </SocialLink>
+        <SocialLink href="https://www.pinterest.com/mohsenpourbanaei" icon={GitHubIcon}>
+          Follow me on GitHub
+        </SocialLink>
+        <SocialLink href="https://discord.gg/" icon={DiscordIcon}>
+          Join the Discord
+        </SocialLink>
+      </div>
+    </div>
+  )
+}
+
+export default MinimalSocialsFooter
