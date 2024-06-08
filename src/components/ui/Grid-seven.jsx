c0b765b4204620718e4f3614240da9ddd19f1979
Mamad Jadali <jadalimamad@gmail.com>
2024-06-08 03:34:27 +0330

    flowbit crousal

--- src/components/ui/Grid-seven.jsx
+++ src/components/ui/Grid-seven.jsx
@@ -1,14 +1,49 @@
-import Ripple from '../magicui/ripple'
 
 const GridSeven = () => {
   return (
     <div className="h-80 overflow-hidden md:col-span-2 md:row-span-1 col-span-3 bg-neutral-50/5 backdrop-blur border border-gray-600/50 rounded-[30px] justify-center items-center rounded-bl-xl rounded-tr-[4px]">
-        <div className="relative flex h-full w-full max-w-full items-center justify-center overflow-hidden bg-background p-20 md:shadow-xl">
-        <p className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-white">
-            Gallery
-        </p>
-        <Ripple />
-        </div>
+      <div id="gallery" class="relative w-full h-full" data-carousel="slide">
+          {/* <!-- Carousel wrapper --> */}
+          <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+              {/* <!-- Item 1 --> */}
+              <div class="duration-700 ease-in-out" data-carousel-item>
+                  <img src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" class="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt=""/>
+              </div>
+              {/* <!-- Item 2 --> */}
+              <div class="duration-700 ease-in-out" data-carousel-item="active">
+                  <img src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" class="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt=""/>
+              </div>
+              {/* <!-- Item 3 --> */}
+              <div class="duration-700 ease-in-out" data-carousel-item>
+                  <img src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" class="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt=""/>
+              </div>
+              {/* <!-- Item 4 --> */}
+              <div class="duration-700 ease-in-out" data-carousel-item>
+                  <img src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" class="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt=""/>
+              </div>
+              {/* <!-- Item 5 --> */}
+              <div class="duration-700 ease-in-out" data-carousel-item>
+                  <img src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" class="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt=""/>
+              </div>
+          </div>
+          {/* <!-- Slider controls --> */}
+          <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+              <span class="inline-flex items-center justify-center w-8 h-8 rounded-full bg-gray-800/30 group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
+                  <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+                  </svg>
+                  <span class="sr-only">Previous</span>
+              </span>
+          </button>
+          <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+              <span class="inline-flex items-center justify-center w-8 h-8 rounded-full bg-gray-800/30 group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white  group-focus:outline-none">
+                  <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                  </svg>
+                  <span class="sr-only">Next</span>
+              </span>
+          </button>
+      </div>
     </div>
   )
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 import defaultTheme from 'tailwindcss/defaultTheme'
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		"./node_modules/flowbite/**/*.js"
+	],
 	theme: {
 		extend: {
 			fontFamily: {
@@ -37,5 +40,7 @@ export default {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		require('flowbite/plugin')
+	],
 }
